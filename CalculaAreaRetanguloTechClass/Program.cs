fa5c106396f777494f4ea4bec6ba0f6c61d0cd68
Cristian Serena <devcristian02@gmail.com>
2024-08-28 21:29:24 -0300

    POO_Retangulo

---
+++ CalculaAreaRetanguloTechClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CalculaAreaRetanguloTechClass
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = new string('.', 45);
+            Console.WriteLine("Programa para Calcular a Área de um Retangulo");
+            Console.WriteLine(sep);
+
+            Retangulo retangulo = new Retangulo();
+
+            Console.WriteLine("Por Favor Digite a Altura do Retangulo: ");
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Por Favor Digite a Largura do Retangulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(sep);
+
+            Console.WriteLine($"O Valor da Área é: {retangulo.CalculaArea():F2}");
+            Console.WriteLine(sep);
+            Console.WriteLine($"O Valor do Perimetro é: {retangulo.CalculaPerimetro():F2}");
+            Console.WriteLine(sep);
+            Console.WriteLine($"O Valor da Diagonal é: {retangulo.CalculaDiagonal():F2}");
+
+        }
+    }
+}
---
+++ CalculaAreaRetanguloTechClass/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CalculaAreaRetanguloTechClass
+{
+    public class Retangulo
+    {
+        public double Altura;
+        public double Largura;
+
+
+
+        public double CalculaPerimetro()
+        {
+             return 2 * (Altura + Largura)  ;
+        }
+
+        public double CalculaArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double CalculaDiagonal()
+        {
+             return Math.Sqrt(Math.Pow(Altura,2) + (Math.Pow(Largura,2)));
+        }
+    }
+
+
+
+}
