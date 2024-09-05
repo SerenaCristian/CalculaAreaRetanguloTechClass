b60533881a0379b3a82ca3a932633fedceb2fb3a
Cristian Serena <devcristian02@gmail.com>
2024-09-04 23:11:23 -0300

    Simplificando gets e sets

--- CalculaAreaRetanguloTechClass/Retangulo.cs
+++ CalculaAreaRetanguloTechClass/Retangulo.cs
@@ -4,57 +4,35 @@ namespace CalculaAreaRetanguloTechClass
 {
     public class Retangulo
     {
-        
-            private double _altura;
-            private double _largura;
+        public double Altura { get; set; }
+        public double Largura { get; set; }
 
-            public double Altura
-            {
-                get { return _altura; }
-                set { _altura = value; }
-            }
-
-            public double Largura
-            {
-                get { return _largura; }
-                set { _largura = value; }
-            }
-
-            public Retangulo(double altura, double largura)
-            {
-                Altura = altura;
-                Largura = largura;
-            }
-
-
-            public double CalculaPerimetro()
-            {
-                return 2 * (Altura + Largura);
-            }
-
-            public double CalculaArea()
-            {
-                return Altura * Largura;
-            }
-
-            public double CalculaDiagonal()
-            {
-                return Math.Sqrt(Math.Pow(Altura, 2) + (Math.Pow(Largura, 2)));
-            }
-
-            public override string ToString()
-            {
-                return $"O Valor da Área é: {CalculaArea():F2}\n" +
-                $"O Valor do Perimetro é: {CalculaPerimetro():F2}\n" +
-                $"O Valor da Diagonal é: {CalculaDiagonal():F2}";
-            }
+        public Retangulo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
         }
 
+        public double CalculaPerimetro()
+        {
+            return 2 * (Altura + Largura);
+        }
+
+        public double CalculaArea()
+        {
+            return Altura * Largura;
+        }
+
+        public double CalculaDiagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+
+        public override string ToString()
+        {
+            return $"O Valor da Área é: {CalculaArea():F2}\n" +
+                   $"O Valor do Perímetro é: {CalculaPerimetro():F2}\n" +
+                   $"O Valor da Diagonal é: {CalculaDiagonal():F2}";
+        }
     }
-
-
-
-
-
-
-
+}
